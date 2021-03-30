5415d0a83e5ab560c79b571905e3d753f53c528a
Benito <marcote@jive.eu>
2021-03-30 11:53:24 +0200

    Fixed quote

--- dog_speak.py
+++ dog_speak.py
@@ -1,10 +1,10 @@
 import argparse
 
 def dog_speak(msg):
-  return f"bark bark, {msg}" 
+  return f"bark bark, {msg}"
 
 def main():
-    parser = argparse.ArgumentParser(description="Print custom message on the screen')
+    parser = argparse.ArgumentParser(description='Print custom message on the screen')
     parser.add_argument("--message", "-m", help="Enter a message that will be printed on the screen")
     args = parser.parse_args()
 
